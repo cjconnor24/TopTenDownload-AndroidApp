66b24d81b554d4fd89c76fa90a8c894dc62aae53
Chris Connor <chris@chrisconnor.co.uk>
2019-02-17 16:11:12 +0000

    Setup the class objects to store the data

---
+++ app/src/main/java/uk/co/chrisconnor/toptendownload/ParseApplications.java
@@ -0,0 +1,18 @@
+package uk.co.chrisconnor.toptendownload;
+
+import java.util.ArrayList;
+
+public class ParseApplications {
+
+    private static final String TAG = "ParseApplications";
+
+    private ArrayList<FeedEntry> applications;
+
+    public ParseApplications() {
+        this.applications = new ArrayList<>();
+    }
+
+    public ArrayList<FeedEntry> getApplications() {
+        return applications;
+    }
+}
