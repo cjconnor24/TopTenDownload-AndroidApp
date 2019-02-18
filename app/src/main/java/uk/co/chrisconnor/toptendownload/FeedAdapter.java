8e184012c591fdc200f89d2198ff8aec181f8627
Chris Connor <chris@chrisconnor.co.uk>
2019-02-18 08:38:50 +0000

    Need to refactor to remove old code

--- app/src/main/java/uk/co/chrisconnor/toptendownload/FeedAdapter.java
+++ app/src/main/java/uk/co/chrisconnor/toptendownload/FeedAdapter.java
@@ -2,6 +2,8 @@ package uk.co.chrisconnor.toptendownload;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import java.util.List;
@@ -18,5 +20,16 @@ public class FeedAdapter extends ArrayAdapter {
         this.layoutResource = resource;
         this.layoutInflater = LayoutInflater.from(context);
         this.applications = applications;
+    }
+
+    @Override
+    public int getCount() {
+        return applications.size();
+    }
+
+    @androidx.annotation.NonNull
+    @Override
+    public View getView(int position, @androidx.annotation.Nullable View convertView, @androidx.annotation.NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
     }
 }
